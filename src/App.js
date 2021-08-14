07b5a1b826f19fb499b2ae330b067bfa9798f129
LnX <luan38.eric@gmail.com>
2021-08-14 20:01:02 -0300

    Construção página de carros

--- src/App.js
+++ src/App.js
@@ -1,25 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import * as S from './styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Car extends Component {
+
+    state ={
+      carAdd:[
+
+      ],
+      car: [
+        {
+          id: 0,
+          name: "Jetta",
+          assembler: "Volkswagen",
+          price: 144000,
+          type: "Sedan"
+        },
+        {
+          id: 1,
+          name: "Polo",
+          assembler: "Volkswagen",
+          price: 70000,
+          type: "Hatch"
+        },
+        {
+          id: 2,
+          name: "T-Cross",
+          assembler: "Volkswagen",
+          price: 123000,
+          type: "SUV"
+        },
+        {
+          id: 3,
+          name: "Tiguan R-line",
+          assembler: "Volkswagen",
+          price: 146000,
+          type: "SUV"
+        },
+        {
+          id: 4,
+          name: "Civic",
+          assembler: "Honda",
+          price: 115000,
+          type: "Sedan"
+        },
+        {
+          id: 5,
+          name: "Corolla",
+          assembler: "Toyota",
+          price: 110000,
+          type: "Sedan"
+        },
+        {
+          id: 6,
+          name: "Corolla Cross",
+          assembler: "Toyota",
+          price: 184000,
+          type: "SUV"
+        },
+        {
+          id: 7,
+          name: "Compass",
+          assembler: "Jeep",
+          price: 132000,
+          type: "SUV"
+        },
+        {
+          id: 8,
+          name: "Golf G TI",
+          assembler: "Volkswagen",
+          price: 138000,
+          type: "Hatch"
+        }
+      ]
+    }
+
+    funcBtnAdd = (event) => {
+      const check = this.state.car.filter(item => {
+        return item.id == event.target.id
+      });
+      this.setState({
+        carAdd: this.state.carAdd.concat(check)
+      })
+      console.log(this.state.carAdd)
+    }
+
+    funcBtnRemove = (event) => {
+      this.setState({
+        carAdd : this.state.carAdd.filter(item => {
+          return item.id != event.target.id
+        })
+      })
+    }
+    render(){
+    return(
+      <S.Container>
+        <S.BoxTitle>
+          <S.Title>Loja de carros!</S.Title>
+        </S.BoxTitle>
+        
+        <S.ChoiceGroup>
+          <S.CarPurchase>
+            {this.state.car.map((item, index) => {
+              return(
+              <S.EnvelopCar>
+                <S.EnvelopTitle>
+                  <S.CarTitle>{item.name}</S.CarTitle>
+                  <S.Btn id={item.id}onClick={(ev)=>this.funcBtnAdd(ev)}>+</S.Btn>
+                </S.EnvelopTitle>
+                <S.BoxCardz>
+                  <S.CarAssembler>
+                    <span>Montadora:</span> {item.assembler}
+                  </S.CarAssembler>
+                
+                    <S.Preco>
+                      <span>Preço:</span> {item.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}
+                    </S.Preco>
+                  
+                    <S.Type>
+                      <span>Tipo:</span> {item.type}
+                    </S.Type>
+                </S.BoxCardz>
+              
+              </S.EnvelopCar>
+  )})}
+          </S.CarPurchase>
+      
+          <S.ChoiceBox>
+            {this.state.carAdd.map((item)=>{
+              return(
+                <S.Group>
+                  <S.BoxNameCar>
+                    <S.Paragraph>{item.name}</S.Paragraph>
+                    <S.Subtraction id={item.id} onClick={(ev) => this.funcBtnRemove(ev)}>-</S.Subtraction>
+                  </S.BoxNameCar>
+                  <S.BoxAbout>
+                    <S.Paragraph>Tipo:{item.price}</S.Paragraph>
+                    <S.Paragraph>Preço:{item.type}</S.Paragraph>
+                  </S.BoxAbout>
+                </S.Group>
+              )
+            })}
+          </S.ChoiceBox>
+         
+        </S.ChoiceGroup>
+
+        <S.BoxResult>
+            <S.Total>Total</S.Total>
+          </S.BoxResult>
+      </S.Container>
+      
+    )
+  }
+  
 }
-
-export default App;
+export default Car;
---
+++ src/styles.js
@@ -0,0 +1,153 @@
+import styled from "styled-components";
+
+export const BoxTitle = styled.div`
+    display: flex;
+    justify-content: center;
+   
+`;
+
+export const Title = styled.h1`
+    color: blue;
+    font-size: 1.5em;
+`;
+
+export const Container = styled.div`
+    width: 100%;
+   
+`;
+
+export const ChoiceGroup = styled.div`
+    display: flex;
+`;
+
+export const CarPurchase = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    width: 70%;
+`;
+
+export const EnvelopTitle = styled.h2`
+    background: rgb(245, 245, 245);
+    font-size: 1.4vw;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    height: 2rem;
+    margin: 0 ;
+    border: solid 1px #d3d3d3;
+    border-radius: 5px 5px 0px 0px ; 
+
+`;
+export const EnvelopCar = styled.div`
+    display: flex;
+    width: 220px;
+    margin-bottom: 5px ; 
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    border: solid 1px #d3d3d3;
+    border-radius: 5px;
+    cursor: pointer;
+    box-shadow: 0px 0px 10px 0px #add8e6;
+    transition: 0.5s all;
+
+    &:hover{
+        box-shadow: none;
+        transform: translateY(2px);
+        border: solid 1px #add8e6;
+    }
+
+    &:hover ${EnvelopTitle}{
+        background: blue;
+    }
+
+`;
+
+
+export const BoxCardz = styled.div`
+
+  
+`;
+
+export const CarTitle = styled.p`
+   
+`;
+
+export const Btn = styled.button`
+    border-radius: 25px;
+    border: none;
+    color: #fff;
+    background: #000;
+    cursor: pointer;
+`;
+
+
+export const CarAssembler = styled.p`
+`;
+
+
+export const Preco = styled.p`
+
+`;
+
+
+export const Type = styled.p`
+
+`;
+
+export const ChoiceBox = styled.div`
+    width: 30%;
+    height: 35vw;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: solid 1px #d3d3d3;
+`;
+
+export const BoxResult = styled.div`
+    display: flex;
+    width: 30%;
+    margin-left: 70%;
+`;
+
+export const Total = styled.h3`
+    font-size: 1.5vw;
+`;
+
+export const Group = styled.div`
+    border: 1px solid blue;
+    width: 80%; 
+    display: flex;
+    flex-direction: column;
+    margin-top: 10px;
+    align-items: top;
+`;
+
+export const Paragraph = styled.p`
+
+`;
+
+export const Subtraction = styled.button`
+    background-color: #fff;
+    color: blue;
+    border: none;
+    border-radius: 10px;
+`;
+
+export const BoxNameCar = styled.div`
+    background: blue;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: #fff;
+    height: 2vw;
+`;
+
+export const BoxAbout = styled.div`
+    display: flex;
+    justify-content: space-evenly; 
+`;
